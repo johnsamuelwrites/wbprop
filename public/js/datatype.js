ebb7c3657f85d8886c1a7f0e3c13e0643de35cd8
John Samuel <johnsamuelwrites@gmail.com>
2026-02-14 22:53:30 +0100

    Remove obselete files

--- public/js/datatype.js
+++
@@ -1,9 +0,0 @@
-export const datatypeQuery = `
-PREFIX wikibase: <http://wikiba.se/ontology#>
-SELECT DISTINCT ?datatype (count(DISTINCT ?item) as ?count)
-WHERE
-{
-    ?item wikibase:propertyType ?datatype.
-}
-GROUP by ?datatype
-`;
--- public/js/property.js
+++
@@ -1,8 +0,0 @@
-export const propertyStatementQuery = `
-SELECT ?property ?statementCount WHERE {
-    ?property rdf:type wikibase:Property;
-      wikibase:statements ?statementCount.
-  }
-  ORDER BY DESC (?statementCount)
-  LIMIT 15
-`;
